b455245a8352d8a47f28528ce3978c54e296c473
Ahmed Mahmud <ahmedmahmud1994@hotmail.com>
2025-04-24 15:51:51 +0000

    brand green color tested

--- swaptrade-frontend/src/app/page.tsx
+++ swaptrade-frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline text-center">
+      <h1 className="text-3xl font-bold underline text-center text-brand-green">
         Welcome to SwapTrade
       </h1>
       <p></p>
